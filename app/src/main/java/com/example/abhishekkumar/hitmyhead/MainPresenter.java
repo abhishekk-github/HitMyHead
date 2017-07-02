ff92ffd14152532280f0d65e416adc050c6bf432
abhishekkumar <abhishek.kumar@ixigo.com>
2017-07-02 23:28:31 +0530

    layout changes

--- app/src/main/java/com/example/abhishekkumar/hitmyhead/MainPresenter.java
+++ app/src/main/java/com/example/abhishekkumar/hitmyhead/MainPresenter.java
@@ -1,8 +1,11 @@
 package com.example.abhishekkumar.hitmyhead;
 
+import android.app.Activity;
+
 /**
  * Created by abhishekkumar on 01/07/17.
  */
 
-public class MainPresenter {
+public class MainPresenter extends Activity {
+
 }

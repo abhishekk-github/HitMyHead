a603b5a0bb5727e2959c69c271266f96e777d06d
abhishekkumar <abhishek.kumar@ixigo.com>
2017-07-09 16:26:55 +0530

    Google map integration for Texi done

--- app/src/main/java/com/example/abhishekkumar/hitmyhead/MainPresenter.java
+++ app/src/main/java/com/example/abhishekkumar/hitmyhead/MainPresenter.java
@@ -7,5 +7,38 @@ import android.app.Activity;
  */
 
 public class MainPresenter extends Activity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
